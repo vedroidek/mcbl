26df3f250716eb440f87a33f3921f3d934fc0900
vedroidek <4droidek@gmail.com>
2025-02-18 00:31:40 +0500

    Add admin table

--- microblog/core/models.py
+++ microblog/core/models.py
@@ -1,8 +1,8 @@
-from datetime import datetime, date
-from typing import List, Annotated
+from datetime import date, datetime
+from typing import Annotated, List
+
 from sqlalchemy import ForeignKey, String, Text, func
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, \
-    relationship
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 
 class Base(DeclarativeBase):
@@ -12,47 +12,57 @@ class Base(DeclarativeBase):
 intpk = Annotated[int, mapped_column(primary_key=True)]
 
 
-class User(Base):
-    
-    __tablename__ = "user_account"
+class Admin(Base):
+    __tablename__ = "admin"
+
+    id: Mapped[intpk]
+    nickname: Mapped[str] = mapped_column(
+        String(10),
+        nullable=False,
+        unique=True,
+    )
+    password: Mapped[str] = mapped_column(String(128), nullable=False)
+
+
+class Author(Base):
+    __tablename__ = "author_account"
 
     id: Mapped[intpk]
     nickname: Mapped[str] = mapped_column(
         String(32), unique=True, nullable=False)
     email_address: Mapped[str] = mapped_column(
         String(64), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(128), nullable=False)
     registered: Mapped[datetime] = mapped_column(
         server_default=func.current_timestamp()
     )
     person_data: Mapped["PersonData"] = relationship(
         back_populates="user", cascade="all, delete-orphan"
     )
-    posts: Mapped[List['Post']] = relationship(
+    posts: Mapped[List["Post"]] = relationship(
         back_populates="user", cascade="all, delete-orphan"
     )
     comments: Mapped[List["Comment"]] = relationship(back_populates="author")
 
     def __repr__(self) -> str:
-        return f"User(id={self.id!r}, fullname={self.nickname!r})"
+        return f"Author(id={self.id!r}, fullname={self.nickname!r})"
 
 
 class PersonData(Base):
-
     __tablename__ = "person_data"
 
     id: Mapped[intpk]
-    user_id: Mapped[int] = mapped_column(ForeignKey("user_account.id"))
+    user_id: Mapped[int] = mapped_column(ForeignKey("author_account.id"))
     gender: Mapped[str]
     birthday: Mapped[date]
-    user: Mapped["User"] = relationship(back_populates="personal_data")
+    user: Mapped["Author"] = relationship(back_populates="personal_data")
 
     def __repr__(self) -> str:
         return f"PersonData(id={self.id!r}, \
             email_address={self.user.nickname!r})"
-    
+
 
 class Post(Base):
-
     __tablename__ = "post"
 
     id: Mapped[intpk]
@@ -61,7 +71,7 @@ class Post(Base):
     created_at: Mapped[datetime] = mapped_column(
         server_default=func.current_timestamp()
     )
-    author_id: Mapped[int] = mapped_column(ForeignKey("user_account.id"))
+    author_id: Mapped[int] = mapped_column(ForeignKey("author_account.id"))
     comments_id: Mapped[List["Comment"]] = relationship(back_populates="post")
 
     def __repr__(self) -> str:
@@ -69,7 +79,6 @@ class Post(Base):
 
 
 class Comment(Base):
-
     __tablename__ = "comment"
 
     id: Mapped[intpk]
@@ -78,8 +87,8 @@ class Comment(Base):
         server_default=func.current_timestamp()
     )
     post_id: Mapped["Post"] = mapped_column(ForeignKey("post.id"))
-    author_id: Mapped[int] = mapped_column(ForeignKey("user_account.id"))
-    author: Mapped["User"] = relationship(back_populates="comment")
+    author_id: Mapped[int] = mapped_column(ForeignKey("author_account.id"))
+    author: Mapped["Author"] = relationship(back_populates="comment")
 
     def __repr__(self):
         return f"Comment(id={self.id!r}, title={self.author.nickname!r})"
