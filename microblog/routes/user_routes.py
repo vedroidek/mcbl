b71b690c0839df3885d96224658e1c7d5d97da16
vedroidek <4droidek@gmail.com>
2025-11-09 14:13:33 +0500

    Deleted

--- microblog/routes/user_routes.py
+++
@@ -1,8 +0,0 @@
-import microblog
-from flask import render_template
-
-
-@app.route("/", methods=["GET"])
-def index():
-    vasya = "Вася"
-    render_template("user/greetengs.html", username=vasya)
