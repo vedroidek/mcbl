f6aa6c5d8daf65bff780099ed1622065c72a5ab1
vedroidek <4droidek@gmail.com>
2025-11-09 14:18:25 +0500

    Add: route handlers. Convenient registration of new Blueprints

--- microblog/__init__.py
+++ microblog/__init__.py
@@ -1,13 +1,19 @@
-import os
 from flask import Flask
+from .config import confdict
+from .routes import comm_bp, user_bp
 
 
-def create_app(test_config=None):
+def create_app(config_name=None):
     app = Flask(__name__)
 
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    if not config_name:
+        app.config.from_object(confdict["dev"])
+    else:
+        app.config.from_object(confdict[config_name])    
+
+    # blueprints
+    app.register_blueprint(user_bp, url_prefix='/user')
+    app.register_blueprint(comm_bp, url_prefix='/')
+
 
     return app
---
+++ microblog/routes/__init__.py
@@ -0,0 +1,8 @@
+from .user_routes.user import user_bp
+from .common import comm_bp
+
+
+__all__ = [
+    "user_bp",
+    "comm_bp",
+    ]
