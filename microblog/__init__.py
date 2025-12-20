a63d04d0a6670361399613d87c762ce0016be525
vedroidek <4droidek@gmail.com>
2025-12-20 22:16:59 +0500

    Before switching to the new "front" branch.

--- microblog/__init__.py
+++ microblog/__init__.py
@@ -1,11 +1,13 @@
 import logging
 from flask import Flask
+from flask_cors import CORS
 from .config import confdict
 from .routes import comm_bp, user_bp
 
 
 def create_app(config_name="default"):
     app = Flask(__name__)
+    CORS(app, origins=["http://localhost:5050"], methods=["GET", "POST"])
 
     app.config.from_object(confdict[config_name])
 
